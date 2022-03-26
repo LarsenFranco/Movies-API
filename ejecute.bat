80baf8cbb92a720d0cc136fb77e236bb1fe76789
Franco <86502031+LarsenFranco@users.noreply.github.com>
2022-03-26 03:10:22 -0300

    Update ejecute.bat

--- ejecute.bat
+++ ejecute.bat
@@ -1,10 +1,12 @@
 @ECHO EJECUTANDO APLICACION MOVIE APP
-COLOR 30
+COLOR 10
 CD "frontend\project"
+START PowerShell.exe -Command "npm i"
 START PowerShell.exe -Command "npm run dev"
 CD ..
 CD ..
 CD "backend"
+START PowerShell.exe -Command "npm i"
 START PowerShell.exe -Command "nodemon app.js"
 START http://localhost:3000/
 
